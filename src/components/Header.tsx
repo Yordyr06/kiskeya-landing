cc6ae2bc21fd48df6d6de8a96e13e8fd5e3556f8
Yordy Almonte <yoalmonte.dev@outlook.com>
2024-01-16 15:55:36 -0400

    Header design ready

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,22 @@
 export const Header = (): JSX.Element => {
   return (
-    <div className="mx-auto max-w-screen-xl p-4 py-8">
-      <h1 className="mb-4">
-        <span className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-pink-300 to-orange-700">
-          Kiskeya Landing
+    <header className="relative flex  w-full min-w-80 justify-between p-4">
+      <section className="relative flex min-w-64 content-center">
+        <span className="my-auto font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-pink-300 to-orange-700">
+          <h1>
+            Kiskeya Landing
+          </h1>
         </span>
-      </h1>
-    </div>
+      </section>
+
+      <section className="min-w-32 flex justify-center">
+        <figure>
+          <img 
+            className="relative w-40 rounded-full" 
+            src="https://yt3.googleusercontent.com/ytc/AIf8zZT696k3dDu6HT2DRHBX0oFZLVNO1dr_nSnUlNEA-A=s176-c-k-c0x00ffffff-no-rj"
+            alt="" />
+        </figure>
+      </section>
+    </header>
   )
 }
