357b424c8ea4ed591fe1c65dcb15ca02cc210484
Yordy Almonte <yoalmonte.dev@outlook.com>
2024-01-16 02:56:45 -0400

    Configuration completed

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export const Header = (): JSX.Element => {
+  return (
+    <div className="mx-auto max-w-screen-xl p-4 py-8">
+      <h1 className="mb-4">
+        <span className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-pink-300 to-orange-700">
+          Kiskeya Landing
+        </span>
+      </h1>
+    </div>
+  )
+}
