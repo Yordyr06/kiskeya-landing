ad74e135164463c784d04e2c527dbdd1e74008f8
Yordy Almonte <yoalmonte.dev@outlook.com>
2024-01-17 05:33:06 -0400

    Axios implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,74 @@
 import React from "react"
+import { useState, useEffect } from "react"
+import axios from "axios"
 import { Header } from "./components/Header"
 import { VideoCard } from "./components/VideoCard"
 import { Footer } from "./components/Footer"
 
+interface Videos {
+  id: string
+  //
+}
+
+export type State = {
+  videos: Videos[],
+  error: unknown,
+  isLoading: boolean
+}
+
 function App() {
+  const [state, setState] = useState<State>({
+    videos: [],
+    error: null,
+    isLoading: true
+  })
+  
+  useEffect(() => {
+    const getVideos = async () => {
+      const options = {
+        method: 'GET',
+        url: 'https://youtube138.p.rapidapi.com/channel/videos/',
+        params: {
+          id: 'UC1gKBgknwlQagEnq4Q-UnCg',
+          filter: 'videos_latest',
+          hl: 'en',
+          gl: 'US'
+        },
+        headers: {
+          'X-RapidAPI-Key': '79549650c5msh124b1a59b60adcdp15c721jsn476c40792292',
+          'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+        }
+      };
+      
+      try {
+        const response = await axios.request<{ items: Videos[] }>(options);
+        setState({ 
+          videos: response.data.items,
+          error: null, 
+          isLoading: false
+        });
+      } catch (error) {
+        console.error('Error al obtener datos de YouTube', error);
+        setState({
+          videos: [],
+          error,
+          isLoading:false
+        });
+      }  
+    }
+  
+    getVideos()
+  }, [])
+
+
   return (
     <>
       <Header />
-      <VideoCard />
+      <VideoCard 
+        videos={ state.videos }
+        error={ state.error }
+        isLoading={state.isLoading}
+      />
       <Footer />
     </>
   )
--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,39 +1,4 @@
-import { useEffect } from 'react';
-import axios from 'axios';
-
-// type Video = {
-
-// }
-
 export const VideoCard = (): JSX.Element => {
-
-  useEffect(() => {
-    const getVideos = async () => {
-      const options = {
-        method: 'GET',
-        url: 'https://youtube138.p.rapidapi.com/channel/videos/',
-        params: {
-          id: 'UC1gKBgknwlQagEnq4Q-UnCg',
-          filter: 'videos_latest',
-          hl: 'en',
-          gl: 'US'
-        },
-        headers: {
-          'X-RapidAPI-Key': '79549650c5msh124b1a59b60adcdp15c721jsn476c40792292',
-          'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-        }
-      };
-      
-      try {
-        const response = await axios.request(options);
-        console.log(response.data);
-      } catch (error) {
-        console.error(error);
-      }  
-    }
-  
-    getVideos()
-  }, [])
   
   return (
     <article className="flex h-44 w-80 justify-center bg-gray-700 rounded-lg mx-auto">
